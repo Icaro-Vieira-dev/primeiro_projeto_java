722d2eef60c89e7a2a8447fa06ea2cfa49a288e3
Icaro Vieira <icarosug@gmail.com>
2026-05-15 02:03:53 -0300

    Meu primeiro projeto Java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import teste.Estudo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+        Estudo.Anotacao("Aprendi Java");
+
+    }
+}
+
+
---
+++ src/teste/Estudo.java
@@ -0,0 +1,8 @@
+package teste;
+
+public class Estudo {
+
+    public static void Anotacao(String texto){
+        System.out.print("Sua anotação: " + texto);
+    }
+}
